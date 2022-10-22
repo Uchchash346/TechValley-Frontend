d4135e62d8067ed088a6535f3be0db1e9a005338
Uchchash346 <mru416@gmail.com>
2022-10-22 22:05:15 +0600

    api structure setup

--- src/components/pages/OfferDeal.js
+++ src/components/pages/OfferDeal.js
@@ -6,15 +6,22 @@ const OfferDeal = () => {
     return (
         <>
             <div class="container mx-auto pt-5">
+            <h2 className="text-sky-800 text-center pb-5 text-4xl uppercase">Get Friday Offers</h2>
                 <div className="grid grid-rows-1 grid-flow-col gap-4">
                     <div>
-                        <img src={OfferImage1} alt="" />
+                        <a href="/">
+                            <img src={OfferImage1} alt="" />
+                        </a>
                     </div>
                     <div>
-                        <img src={OfferImage2} alt="" />
+                        <a href="/">
+                            <img src={OfferImage2} alt="" />
+                        </a>
                     </div>
                     <div>
-                        <img src={OfferImage3} alt="" />
+                        <a href="/">
+                            <img src={OfferImage3} alt="" />
+                        </a>
                     </div>
                 </div>
             </div>
